0e5ea7d74f2b27eae28141a8cfdac6d5e856eb83
Prashantha <prashantha.g@gmail.com>
2015-02-16 00:01:58 -0600

    * definition to mac

--- ptest.py
+++ ptest.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 #import os
-from subprocess import Popen, PIPE
 ip_list=["192.168.0.1", "192.168.0.103"]
 c=3
 r=3
-for ss in ip_list:
+roundVal=3
+def extractMAC(ss):
 	ss=ss.strip('"')
 	pid = Popen(["arp", "-a", ss], stdout=PIPE)
 	s = pid.communicate()[0]
@@ -14,11 +14,20 @@ for ss in ip_list:
 	for f in range(3):input.next()
 	mn=input.next()
 	mac=mn.split()[1].replace("-","")
-	print mac
+	return mac
+	
+from subprocess import Popen, PIPE
 
 
-csec = 1.0/c
-rsec = 1.0/c
+if (c*r)!=len(ip_list):
+	print "required devices is ",c*r,"but you entered ",len(ip_list)," ip devices"
+	#return
+for ss in ip_list:
+	print extractMAC(ss);
+
+
+csec = round(1.0/c,roundVal)
+rsec = round(1.0/c,roundVal)
 print csec, rsec
 cleft=[]
 cright=[]
@@ -27,10 +36,10 @@ cbottom=[]
 for i in range(r):
     for j in range(c):
         #cleft[i*c+j]    = j*csec
-        cleft.append(j*csec)
-        cright.append(1.0-(j+1)*csec)
-        ctop.append(i*rsec)
-        cbottom.append(1.0-(i+1)*rsec)
+        cleft.append(round((j*csec),roundVal))
+        cright.append(round((1.0-(j+1)*csec),roundVal))
+        ctop.append(round((i*rsec),roundVal))
+        cbottom.append(round((1.0-(i+1)*rsec),roundVal))
 
 #for m in range(r*c):
 print  cleft,"\n",cright,"\n",ctop,"\n",cbottom,"\n"
